3fc7c24d44c8f6d3063db440efa7aac6b6d4316e
igorgcf <igorTecnologia@outlook.com>
2025-02-25 15:06:59 -0300

    Cores (util)

---
+++ util/Cores.java
@@ -0,0 +1,10 @@
+package util;
+
+public class Cores {
+
+	public static final String TEXT_RESET = "\u001B[0m";
+	
+	public static final String TEXT_RED = "\u001B[31m";
+	
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+}
